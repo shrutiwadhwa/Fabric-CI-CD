d2a5805513fdc9b17dda1081131fa8caaf4a3367
shrutiwadhwa <shrutiwadhwa99@gmail.com>
2026-05-24 17:39:12 +0530

    Committing 2 items from workspace b79aed05-325a-458e-8274-26078b75c2ff

---
+++ Notebook 23.Notebook/notebook-content.py
@@ -0,0 +1,69 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "d256322c-0315-4467-9af2-f77465c8e3e5",
+# META       "default_lakehouse_name": "cicd",
+# META       "default_lakehouse_workspace_id": "b79aed05-325a-458e-8274-26078b75c2ff",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "d256322c-0315-4467-9af2-f77465c8e3e5"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+sales_data=[
+    (1,"laptop",50000),
+    (2," ")
+]
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+print("shruti")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
